dbd627d7f6c2f6ecb8fcbc727587701b95834ad0
rruiz1982 <ruivia@hotmail.com>
2024-11-10 09:23:57 -0300

    estrucutura ventas y precio de producto

---
+++ Integrador/src/main/java/com/backendIntegrador/controller/SaleController.java
@@ -0,0 +1,38 @@
+package com.backendIntegrador.controller;
+
+import com.backendIntegrador.model.Sale;
+import com.backendIntegrador.service.impl.SaleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/private/sales")
+@RequiredArgsConstructor
+public class SaleController {
+
+    @Autowired
+    private SaleService saleService;
+
+    @PostMapping("")
+    public Sale create (@RequestBody Sale sale) throws Exception {
+        return saleService.save(sale);
+    }
+    @GetMapping("/{id}")
+    public Sale getsale (@PathVariable String id) throws Exception {
+        return saleService.getSaleById(id);
+    }
+    @GetMapping("")
+    public List<Sale> saleList() throws Exception {
+        return saleService.saleList();
+    }
+    @DeleteMapping("/{id}")
+    public void deleteSale (@PathVariable String id) throws Exception {
+        saleService.delete(id);
+    }
+
+    
+}
