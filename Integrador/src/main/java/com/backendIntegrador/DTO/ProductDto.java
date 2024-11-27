9406c0517deca1473031bc2ab7e166dc6e1042f1
Julieta BACHUR <jbachur@CNP.com.ar>
2024-11-27 00:13:24 -0300

    cambio productid del product dto de Long a string

--- Integrador/src/main/java/com/backendIntegrador/DTO/ProductDto.java
+++ Integrador/src/main/java/com/backendIntegrador/DTO/ProductDto.java
@@ -18,7 +18,7 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDto {
-    private Long productId;
+    private String productId;
     private String productName;
     private String thumbnail;
     private Integer Amount;
