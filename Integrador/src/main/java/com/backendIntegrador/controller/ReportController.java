cc8600b50a25bd0fa63793042ec3e5da4de5989e
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2024-12-07 18:34:34 -0300

    Se agrega reporte en pdf para ventas por fechas

--- Integrador/src/main/java/com/backendIntegrador/controller/ReportController.java
+++ Integrador/src/main/java/com/backendIntegrador/controller/ReportController.java
@@ -3,11 +3,14 @@ package com.backendIntegrador.controller;
 import com.backendIntegrador.report.PDFReportGenerator;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("api/v1/admin/report")
@@ -28,6 +31,22 @@ public class ReportController {
 
             // Obtener el flujo de salida y generar el reporte
             pdfReportGenerator.generateAmountProductsByCategory(response.getOutputStream());
+            response.getOutputStream().flush();
+        } catch (Exception e) {
+            throw new RuntimeException("Error al generar el reporte", e);
+        }
+    }
+
+    @GetMapping("/sales")
+    public void generateSalesReport(HttpServletResponse response, @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+                                    @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+        try {
+            // Configurar respuesta como PDF
+            response.setContentType("application/pdf");
+            response.setHeader("Content-Disposition", "inline; filename=reporte_ventas.pdf");
+
+            // Obtener el flujo de salida y generar el reporte
+            pdfReportGenerator.generateSalesTableReport(response.getOutputStream(), startDate, endDate);
             response.getOutputStream().flush();
         } catch (Exception e) {
             throw new RuntimeException("Error al generar el reporte", e);
